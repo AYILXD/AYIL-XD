b1e736e6a949b500c25fd16270fdf4e42975fad2
AYILXD <140167111+AYILXD@users.noreply.github.com>
2023-07-21 19:52:44 +0800

    Update MASSAL.py

--- MASSAL.py
+++ MASSAL.py
@@ -1,1 +1,10 @@
-	
+pkg install update
+pkg install upgrade
+pkg install python
+pip install rich
+pip install requests
+pip install bs4 
+pip install mechanize
+git pull
+cd MASSAL.py
+python MASSAL.py
